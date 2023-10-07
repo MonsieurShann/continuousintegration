d9ffa82c51dc7924dd9f28331070ac0e5a47da8e
MonsieurShann <shannsagouma.ss@gmail.com>
2023-10-07 19:55:30 +0200

    Python script to check if xhci is available

---
+++ test_probe.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+
+size = len(sys.argv)
+
+searched_drivers = ["xhci_hcd", "crc64"]
+
+def search_string_in_dmesg(searched_drivers):
+    dmesg_output = subprocess.check_output(["sudo", "lsmod"]).decode("utf-8")
+    if searched_drivers[0] in dmesg_output:
+        print("Trouve!!!")
+    else:
+        return -1
+
+search_string_in_dmesg(searched_drivers)
+
