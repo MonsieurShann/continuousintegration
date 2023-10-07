b866dc10b58e02c93611d3517cc61abd18464b51
MonsieurShann <shannsagouma.ss@gmail.com>
2023-10-07 23:55:01 +0200

    update

--- install_env3.sh
+++ install_env3.sh
@@ -1,4 +1,8 @@
+cd /opt/var-fslc-yocto/
+echo "SHAAAAAAAAAANNN----------------- 1>"
+ls -lstr
 MACHINE=imx8mq-var-dart DISTRO=fslc-wayland . setup-environment build_wayland
 echo y
-cd ../
+echo "SHAAAAAAAAAANNN----------------- 2 >"
+cd /opt/var-fslc-yocto
 source setup-environment build_wayland
