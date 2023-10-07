fc2866a0191d033d3db52d011b5be4a2a333b331
Shann <81802376+MonsieurShann@users.noreply.github.com>
2023-10-07 21:50:54 +0200

    Update test_probe.py

--- test_probe.py
+++ test_probe.py
@@ -4,10 +4,10 @@ def check_xhci_hcd():
     try:
         # Exécutez la commande 'lsmod' pour lister les modules chargés et recherchez 'xhci_hcd'
         output = subprocess.check_output(['lsmod']).decode('utf-8')
-        if 'xhci_hcd' in output:
-            print("Le module xhci_hcd a été chargé.")
+        if 'watchdog' in output:
+            print("Le module watchdog a été chargé.")
         else:
-            raise Exception("Le module xhci_hcd n'a pas été chargé.")
+            raise Exception("Le module watchdog n'a pas été chargé.")
     except Exception as e:
         print(f"Erreur : {e}")
         exit(1)
