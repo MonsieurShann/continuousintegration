f46377221249236ea9d7dc2bb9b443bc35eb15bd
MonsieurShann <shannsagouma.ss@gmail.com>
2023-10-07 22:18:14 +0200

    prepare the installation environment

---
+++ install_env.sh
@@ -0,0 +1,11 @@
+sudo apt-get install gawk wget git diffstat unzip texinfo gcc-multilib \
+ build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+ xz-utils debianutils iputils-ping libsdl1.2-dev xterm libyaml-dev libssl-dev
+ 
+ sudo apt-get install autoconf libtool libglib2.0-dev libarchive-dev \
+ sed cvs subversion coreutils texi2html docbook-utils python-pysqlite2 \
+ help2man make gcc g++ desktop-file-utils libgl1-mesa-dev libglu1-mesa-dev \
+ mercurial automake groff curl lzop asciidoc u-boot-tools dos2unix mtd-utils pv \
+ libncurses5 libncurses5-dev libncursesw5-dev libelf-dev zlib1g-dev bc rename
+ 
+ sudo apt-get install python3-git 
